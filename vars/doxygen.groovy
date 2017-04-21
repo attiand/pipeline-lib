89da4f3ba94c4fd0e2169905077310ebd82030ba
Mattias Andersson <mattias.j.andersson@gmail.com>
2017-04-21 18:49:26 +0200

    qwe

---
+++ vars/doxygen.groovy
@@ -0,0 +1,36 @@
+logParserRules = '''
+error /:\\s+error:/
+warning /:\\s+warning:/
+'''
+
+ def withArguments(docker, body) {
+         def config = [:]
+         body.resolveStrategy = Closure.DELEGATE_FIRST
+         body.delegate = config
+         body()
+
+         assert config.srcdir && config.doxydir && config.outdir
+
+         sh 'rm -f -r doxygen'
+         sh 'mkdir -p doxygen/cfg'
+         sh "cp $config.doxydir/* doxygen/cfg"
+
+         def template = readFile("$config.doxydir/doxyfile.conf")
+
+         def cfg = template.replaceFirst(/(?m)^INPUT\s*=.*/, "INPUT=$config.srcdir").
+                         replaceFirst(/(?m)^OUTPUT_DIRECTORY\s*=.*/, 'OUTPUT_DIRECTORY=doxygen').
+                         replaceFirst(/(?m)^LAYOUT_FILE\s*=.*/, 'LAYOUT_FILE=doxygen/cfg/doxygenlayout.xml')
+
+         writeFile(file: 'doxygen/cfg/doxyfile.conf', text: cfg)
+
+         try {
+                 //docker.image("nya-docker-registry.its.umu.se/build-doxygen:0.0.1").inside {  sh "doxygen doxygen/cfg/doxyfile.conf" }
+
+                 writeFile(file: 'jenkins-rule-logparser', text: logParserRules)
+         }
+         finally {
+                 step([$class: 'LogParserPublisher', failBuildOnError: true,  unstableOnWarning: true, useProjectRule: true, projectRulePath: 'jenkins-rule-logparser'])
+         }
+
+         sh "cp -r doxygen/html $config.outdir"
+ }
