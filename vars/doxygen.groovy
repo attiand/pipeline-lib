2942fbd97bd3a8591d8f7132d7a7bc60c23de7f6
Mattias Andersson <mattias.j.andersson@gmail.com>
2017-04-21 19:48:17 +0200

    take two

--- vars/doxygen.groovy
+++ vars/doxygen.groovy
@@ -13,17 +13,7 @@ warning /:\\s+warning:/
 
          sh 'rm -f -r doxygen'
          sh 'mkdir -p doxygen/cfg'
-         /*
-         sh "cp $config.doxydir/* doxygen/cfg"
-
-         def template = readFile("$config.doxydir/doxyfile.conf")
-
-         def cfg = template.replaceFirst(/(?m)^INPUT\s*=.*/, "INPUT=$config.srcdir").
-                         replaceFirst(/(?m)^OUTPUT_DIRECTORY\s*=.*/, 'OUTPUT_DIRECTORY=doxygen').
-                         replaceFirst(/(?m)^LAYOUT_FILE\s*=.*/, 'LAYOUT_FILE=doxygen/cfg/doxygenlayout.xml')
-
-         writeFile(file: 'doxygen/cfg/doxyfile.conf', text: cfg)
-         */
+      
          try {
                  docker.image("nya-docker-registry.its.umu.se/build-doxygen:0.0.1").inside {
                    sh "doxygen doxygen/cfg/doxyfile.conf"
