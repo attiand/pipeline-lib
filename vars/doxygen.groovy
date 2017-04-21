863f34fea0aaa4c51911a042950b46b0f172ad03
Mattias Andersson <mattias.j.andersson@gmail.com>
2017-04-21 19:41:13 +0200

    call

--- vars/doxygen.groovy
+++ vars/doxygen.groovy
@@ -3,7 +3,7 @@ error /:\\s+error:/
 warning /:\\s+warning:/
 '''
 
- def withArguments(body) {
+ def call(Closure body) {
          def config = [:]
          body.resolveStrategy = Closure.DELEGATE_FIRST
          body.delegate = config
